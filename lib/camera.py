cd5588303d07c79d6859353fe610836fd646bcec
joxer <diego.luca.candido@gmail.com>
2014-08-25 00:38:35 +0200

    added flask. added opencv

--- lib/camera.py
+++ lib/camera.py
@@ -2,7 +2,10 @@ import picamera
 import os
 import time
 import functools
-
+import io
+import time
+import cv2
+import numpy as np
 class PIC:
 
     def __init__(self, cameraconf):
@@ -11,8 +14,9 @@ class PIC:
     def open_close_camera(func):
         def inner(*args, **kwargs):
             args[0].open_camera()
-            func(*args)
+            value = func(*args)
             args[0].close_camera()
+            return value
         return inner
 
     @open_close_camera
@@ -20,6 +24,27 @@ class PIC:
         self.camera.resolution = (int(self._config['resolution_x']), int(self._config['resolution_y']))
         filename = self.__getNextFileNameImage(self._config['directory'])
         self.camera.capture(filename, 'png',False)
+        return filename
+
+    @open_close_camera
+    def get_cv(self):
+        self.camera.resolution = (int(self._config['resolution_x']), int(self._config['resolution_y']))
+        filename = self.__getNextFileNameImage(self._config['directory'])
+        stream = io.BytesIO()
+        self.camera.capture(stream, format='jpeg')
+        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        image = cv2.imdecode(data, 1)
+        sobel = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
+        cv2.imwrite(filename,sobel)
+        return filename
+
+
+    @open_close_camera
+    def get_raw_picture(self):
+        self.camera.resolution = (int(self._config['resolution_x']), int(self._config['resolution_y']))
+        filename = self.__getNextFileNameImageRaw(self._config['directory'])
+        self.camera.capture(filename, 'rgb',False)
+        return filename
 
     @open_close_camera
     def get_video(self):
@@ -27,6 +52,7 @@ class PIC:
         self.camera.start_recording(filename)
         self.camera.wait_recording(self._config['wait_recording'])
         self.camera.stop_recording()
+        return filename
 
     @open_close_camera
     def get_continous_pictures(self,number):
@@ -42,7 +68,10 @@ class PIC:
         if(os.path.isdir(fullpath)):
             return fullpath +"/"+time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())+".png" 
 
-                
+    def __getNextFileNameImageRaw(self,directory):
+        fullpath = os.path.realpath(directory)
+        if(os.path.isdir(fullpath)):
+            return fullpath +"/"+time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())+".jpg"
                                        
     def __getNextFileNameVideo(self,directory):
         fullpath = os.path.realpath(directory)
@@ -57,6 +86,8 @@ class PIC:
             
     def open_camera(self):
         self.camera = picamera.PiCamera()
+        self.camera.exposure_mode = 'auto'
+        self.camera.awb_mode = 'auto'
         self.camera.led = True
 
     def close_camera(self):
--- main.py
+++ main.py
@@ -2,12 +2,45 @@ from lib.config import *
 from lib.camera import * 
 from lib.switches import * 
 
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
 import time
+
+app = Flask(__name__)
+conf = Config("camera.conf")
+switches = Switches()
+pic = PIC(conf.get_dictionary())
+
+
+app.config.from_object(__name__)
+
+@app.route('/')
+def show_entries():
+
+    filelist = os.listdir(os.getcwd())
+    filelist = filter(lambda x: not os.path.isdir(x), filelist)
+    newest = max(filelist, key=lambda x: os.stat(x).st_mtime)
+
+    return render_template('index.html', path=newwest)
+
+@app.route('/take_photo')
+def do_photo():
+    filename = pic.get_picture()
+    filename = filename.replace(conf.get_dictionary()['directory']+"/","")
+    return render_template('image.html', path=filename)
+
+@app.route('/take_cv')
+def do_cv2():
+    filename = pic.get_cv()
+    filename = filename.replace(conf.get_dictionary()['directory']+"/","")
+    return render_template('image.html', path=filename)
+
+
+@app.route('/take_video')
+def do_video():
+    filename = pic.get_video()
+    return render_template('video.html', path=filename)
+
 if __name__ == '__main__':
-    conf = Config("camera.conf")
-    switches = Switches()
-    pic = PIC(conf.get_dictionary())
-    while True:
-        time.sleep(0.1)
-        switches.get_value(pic.get_picture)
-        
+    app.run(host="0.0.0.0")
